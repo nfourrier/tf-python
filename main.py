8301cc0e47b2663915d5830b1a9d8cf124815a7f
nfourrier <nfourrier@gmail.com>
2017-05-10 17:49:42 +1200

    add train option is main

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ head = 'example'
 filename_cfg = os.path.join('{}.cfg'.format(head))
 weights = '{}.hdf5'.format(head)
 filename_weights = os.path.join('weights',weights)
+train = False
+
+
 
 ## Forward pass
 detector = Detector(filename_cfg,GPU=True)
 detector.load_framework(framework)
-layer_output = detector.eval_layer(layer,img_input)
+
+
+if(train):
+	print("Not implemented")
+else:
+	layer_output = detector.eval_layer(layer,img_input)
