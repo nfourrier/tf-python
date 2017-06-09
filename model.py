18a5a9b04aecdf2a5fd33351fcb891ca2fc990a5
nfourrier <nfourrier@gmail.com>
2017-06-09 16:39:53 +1200

    remove vgg class as it can be directly implemented in cfg file

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-
-
 import dasakl.nn.layer as lay
+
 class mmodel(object):
     def __init__(self):
         '''
@@ -64,7 +63,7 @@ class mmodel(object):
 
 
         N_layer = 0
-        # print(inp[0].get_shape(),'input')
+
         self._layers_list.append([self.inp.name,self.inp,'input'])
         self._layers['input'] = self.inp
         for layer in layers:
@@ -81,12 +80,6 @@ class mmodel(object):
             self._layers_list.append([tf_layer.out.name,tf_layer.out,'{}_{}'.format(param[0],param[1])])
             
             N_layer += 1
-
-
-            # if(param[0]=='fullyconnected'):
-            #     self._layers['flatten_input'] = tf_layer.flatten[0]
-            #     self._layers['flatten_output'] = tf_layer.flatten[1]
-                # self._layers['flatten_output_transpose'] = tf_layer.flatten[1]
         self.out = tf.identity(tf_layer.out,name='output')
         self._layers['output'] = self.out
         self._layers_list.append([self.out.name,self.out,'output'])
@@ -97,7 +90,6 @@ class mmodel(object):
             
             self._variables[var.name] = var
 
-        # return self.inp,tf_layer.out,self._variables
         return self.inp,self.out,self._variables
 
     @property
@@ -131,105 +123,11 @@ class mmodel(object):
         if(isinstance(layers,type(None))):
             layers = self._variables.keys() 
         
-
         for name in layers:
             gp,dat = name.split("/")
-           
-            # print(name,gp,dat,var_dict[name].get_shape(),f[gp][dat].shape)
-            # print(name,f[gp][dat].value.mean())
-            # print(name)
-            # print(f[gp][dat].value.mean())
-            # print(f[gp][dat].value)
-            # print(name,np.prod(f[gp][dat].value.shape),self._variables[name].get_shape(),self._variables[name],f[gp][dat].value.dtype)
-
             session.run(self._variables[name].assign(f[gp][dat].value))
-            # print(f[gp][dat].value.mean())
             
         f.close()
         print('\t ... weights loaded')
-        # exit()
-
-
-class tf_vgg(tf_model):
-    def __init__(self,N_classes):
-        self.N_classes = N_classes
-        if(N_classes==21):
-            self.fc_size = 256
-        else:
-            self.fc_size = 4096
-        self._layers = {}
-
-    def get_training_model(self):
-        layers = {}
-        x = tf.placeholder(tf.float32, [None, 224, 224, 3])
-
-        layers['conv1_1'] = self.conv_layer(x, 3, 64, "conv1_1",trainable=False)
-        layers['conv1_2'] = self.conv_layer(layers['conv1_1'], 64, 64, "conv1_2",trainable=False)
-        layers['pool1'] = self.max_pool(layers['conv1_2'], 'pool1')
-
-        layers['conv2_1'] = self.conv_layer(layers['pool1'], 64, 128, "conv2_1",trainable=False)
-        layers['conv2_2'] = self.conv_layer(layers['conv2_1'], 128, 128, "conv2_2",trainable=False)
-        layers['pool2'] = self.max_pool(layers['conv2_2'], 'pool2')
-
-        layers['conv3_1'] = self.conv_layer(layers['pool2'], 128, 256, "conv3_1",trainable=False)
-        layers['conv3_2'] = self.conv_layer(layers['conv3_1'], 256, 256, "conv3_2",trainable=False)
-        layers['conv3_3'] = self.conv_layer(layers['conv3_2'], 256, 256, "conv3_3",trainable=False)
-        # layers['conv3_4'] = self.conv_layer(layers['conv3_3'], 256, 256, "conv3_4")
-        layers['pool3'] = self.max_pool(layers['conv3_3'], 'pool3')
-
-        layers['conv4_1'] = self.conv_layer(layers['pool3'], 256, 512, "conv4_1",trainable=False)
-        layers['conv4_2'] = self.conv_layer(layers['conv4_1'], 512, 512, "conv4_2",trainable=False)
-        layers['conv4_3'] = self.conv_layer(layers['conv4_2'], 512, 512, "conv4_3",trainable=False)
-        # layers['conv4_4'] = self.conv_layer(layers['conv4_3'], 512, 512, "conv4_4")
-        layers['pool4'] = self.max_pool(layers['conv4_3'], 'pool4')
-
-        layers['conv5_1'] = self.conv_layer(layers['pool4'], 512, 512, "conv5_1",trainable=False)
-        layers['conv5_2'] = self.conv_layer(layers['conv5_1'], 512, 512, "conv5_2",trainable=False)
-        layers['conv5_3'] = self.conv_layer(layers['conv5_2'], 512, 512, "conv5_3",trainable=False)
-        # layers['conv5_4'] = self.conv_layer(layers['conv5_3'], 512, 512, "conv5_4")
-        layers['pool5'] = self.max_pool(layers['conv5_3'], 'pool5')
-
-
-        layers['fc6'] = self.fc_layer(layers['pool5'], 25088, self.fc_size, "fc6",trainable=True)  # 25088 = ((224 / (2 ** 5)) ** 2) * 512
-        layers['fc6'] = tf.nn.dropout(layers['fc6'], 0.5)
-
-        layers['fc7'] = self.fc_layer(layers['fc6'], self.fc_size, self.fc_size, "fc7",trainable=True)
-        layers['fc7'] = tf.nn.dropout(layers['fc7'], 0.5)
-
-        layers['fc8'] = self.fc_layer(layers['fc7'], self.fc_size, self.N_classes, "fc8",trainable=True)
-        y = layers['fc8']
-
-
-        # layers['deconv'] = tf.nn.conv2d_transpose(layers['fc7'], weights, output_shape,
-        #                                     strides=strides, padding='SAME')
-
-        self._layers = layers
-        params = {}
-        for var in tf.all_variables():
-            # print(var.name)
-            params[var.name] = var
-        self._variables = params
-        return x, y,params
-
-
-    @property
-    def layers(self):
-        return self._layers
-
-    @property
-    def variables(self):
-        return self._variables
-
-    def get_loss(self,y,y_):
-        cross_entropy = -y_ * tf.log(tf.nn.softmax(y))
-        return tf.reduce_mean(cross_entropy)
-
-    def get_optimizer(self,loss,lr):
-        optimizer = tf.train.AdagradOptimizer(
-            learning_rate=lr,
-            initial_accumulator_value=0.1,
-            use_locking=False,
-            name='Adagrad')
-        return optimizer.minimize(loss)
 
 
